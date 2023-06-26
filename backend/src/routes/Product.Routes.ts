8be117b543059cf3af3f37d611f7fb53e19a4616
Júlio Adler <julioader@gmail.com>
2023-06-26 00:32:09 -0300

    feat: inicia testes product

--- backend/src/routes/Product.Routes.ts
+++ backend/src/routes/Product.Routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import ProductController from '../controllers/Product.Controller';
+import mdwsProd from '../middlewares/Product.Middleware';
+import mdwsUser from '../middlewares/User.Middleware';
 
 const productRouter = Router();
 const productController = new ProductController();
 
 productRouter
   .get('/', productController.getAll)
-  .post('/', productController.create)
+  .post('/',mdwsProd.validCreateProd, productController.create)
   .put('/', productController.update)
-  .delete('/', productController.delete)
+  .delete('/', mdwsUser.validAdmin, productController.delete)
 
 export default productRouter;
