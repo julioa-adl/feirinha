ba90bec0aa097d36eb9a8bbf168f8fb14a0f2160
julioa-adl <julioader@gmail.com>
2023-07-18 09:46:57 -0300

    brinks: apenas brincando com o next ainda

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Login() {
+    return (
+        <>
+            <h1>Login</h1>
+            <Link href={"/"} className='text-blue-600'>Home</Link>
+        </>
+    )
+}
