756c770b5e7fe50970ba8a6614acca6166e606f4
julioa-adl <julioader@gmail.com>
2023-11-03 21:48:30 -0300

    teste 2 build

--- src/pages/Product/components/CallProdButton.tsx
+++ src/pages/Product/components/CallProdButton.tsx
@@ -11,7 +11,7 @@ const CallProdButton = () => {
     <div className='rounded-full p-2 bg-yellow-400 dark:bg-gray-500
     ease-in-out duration-300 hover:bg-yellow-500 dark:hover:bg-yellow-600
     hover:text-gray-900 dark:hover:text-gray-100
-    fixed bottom-5 right-5 cursor-pointer text-gray-500'
+    fixed bottom-5 right-5 cursor-pointer text-gray-500 dark:text-gray-100'
     onClick={() => setShowProd('register')}
     >
       <PlusIcon className='h-6'
