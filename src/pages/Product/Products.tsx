0cc8f59dfd2909b52414fcd6135e961cbcce3471
julioa-adl <julioader@gmail.com>
2023-11-05 19:22:59 -0300

    teste build

--- src/pages/Product/Products.tsx
+++ src/pages/Product/Products.tsx
@@ -27,7 +27,7 @@ const Products = () => {
   const filterProd = products && productsSort.filter(prodF => ((`${prodF.name} ${prodF.subName} ${prodF.size}${prodF.unitMeasure}`).toLowerCase().includes(search.produto.toLowerCase() || '')))
 
   return(
-    <div className='bg-white h-screen dark:bg-gray-900 overflow-hidden'>
+    <div className='bg-white h-screen dark:bg-gray-900'>
       <div className='pt-5 flex items-center w-full justify-center'>
         <Navigator />
         <Search searching={'produto'}/>
