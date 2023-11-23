29b41ab6974491e4f525f10af0a1ff9b92ccf8ca
julioa-adl <julioader@gmail.com>
2023-11-23 16:46:28 -0300

    teste

--- src/pages/Feirinha/components/Feirinha-Details/FeirinhaDetails.tsx
+++ src/pages/Feirinha/components/Feirinha-Details/FeirinhaDetails.tsx
@@ -49,7 +49,7 @@ const FeirinhaDetails = () => {
   };
 
   return (
-    <div className="bg-white h-screen dark:bg-gray-900">
+    <div className="bg-white h-screen dark:bg-gray-900 overflow-hidden">
       <div className='p-5 flex items-center w-full justify-between'>
         <Navigator />
         {
