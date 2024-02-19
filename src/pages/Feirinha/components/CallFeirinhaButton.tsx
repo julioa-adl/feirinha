63ed4c706115692a4ffb7d92ea8e16b49aac3d05
julioa-adl <julioafmesquita@gmail.com>
2024-02-19 16:30:47 -0300

    aaa

--- src/pages/Feirinha/components/CallFeirinhaButton.tsx
+++ src/pages/Feirinha/components/CallFeirinhaButton.tsx
@@ -8,8 +8,8 @@ const CallFeirinhaButton = () => {
   } = useContext(context);
 
   return(
-    <div className='rounded-xl flex flex-col justify-center items-center w-full h-16 md:h-20 p-2 bg-gray-100 dark:bg-gray-700
-    ease-in-out duration-300 hover:bg-gray-200 dark:hover:bg-gray-800
+    <div className='rounded-xl flex flex-col justify-center items-center w-full md:w-1/2 h-16 md:h-20 p-2 bg-gray-100 dark:bg-gray-800
+    ease-in-out duration-300 hover:bg-gray-200 dark:hover:bg-gray-700
     hover:text-gray-900 dark:hover:text-gray-100
     cursor-pointer text-gray-500 dark:text-gray-100'
     onClick={() => setShowFeirinha('register')}
